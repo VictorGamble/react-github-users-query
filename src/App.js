cfe0f030c85e05f813991c5f85e006ecb5bba391
Victor <Victor.Gamble@outlook.com>
2020-04-01 11:56:37 -0400

    refactored handleSubmit functions

--- src/App.js
+++ src/App.js
@@ -5,17 +5,14 @@ import './App.css';
  class App extends Component {
   state={
     input: "",
-    data: []
+    data: [],
   }
 
 dataLoad = async (url) => {
 try {
-let url = `https://api.github.com/users/[username]`
 const response = await fetch(url)
 const data = await response.json();
-console.log(data)
-return data;
-  
+return data; 
 } catch (error) {
   console.error("Error is =>", error);
   return error;
@@ -24,23 +21,33 @@ return data;
 
 changeHandler = (event) =>{
     this.setState({
-       input:event.target.value
+       input: event.target.value
     })
 }
+
 submitHandler = async (event) => {
 event.preventDefault();
 const {input} = this.state;
 let url = `https://api.github.com/users/${input}`
-const {data} = await this.dataLoad(url);
-this.setState({
-  input: "",
-  data: [...data, input ]
-})
+const userData = await this.dataLoad(url);
+try {
+  this.setState({
+    input: '',
+    data: [...this.state.data, userData]
+  })
+  
+} catch (error) {
+  console.error("error is ", error);
+  return error
+
+}
 }
   
-  render(){
+  render()
+  {
   const {input} = this.state;
   const {data} = this.state
+  console.log('Data is', data)
     return (
     <div className="App">
       <form onSubmit={this.submitHandler}>
